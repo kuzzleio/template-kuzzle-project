2ecdf26ec2fb538fd1f9540364c1a0b29e4487df
azagaya <azagaya.games@gmail.com>
2024-04-27 21:23:20 -0300

    Add hermes plugin

--- app.ts
+++ app.ts
@@ -1,71 +1,54 @@
+/* eslint-disable sort-keys */
 import { MyApplication } from "./lib/MyApplication";
-import { CustomUser } from './lib/local-users-handling/validate-users';
-import { addPipeBeforeCreateRestrictedUser } from './lib/local-users-handling/pipeCreateRestrictedUser';
-import { addPipeAfterCreateRestrictedUser } from './lib/local-users-handling/pipeCreateRestrictedUser';
+import { CustomUser } from "./lib/local-users-handling/validate-users";
+import { addPipeBeforeCreateRestrictedUser } from "./lib/local-users-handling/pipeCreateRestrictedUser";
+import { addPipeAfterCreateRestrictedUser } from "./lib/local-users-handling/pipeCreateRestrictedUser";
 
+import fs from "fs";
 
-const fs = require('fs');
-
-const key_secret = JSON.parse(fs.readFileSync('./.env.json', 'utf-8'));
-
-// const key_secret = require("./.env.json");
-
-// console.log(key_secret.clientsecret)
-
-
-console.log(key_secret.clientsecret)
+const env = JSON.parse(fs.readFileSync("./.env.json", "utf-8"));
 const app = new MyApplication();
-
 
 addPipeBeforeCreateRestrictedUser(app);
 //addPipeBeforeCreateUser(app);
 addPipeAfterCreateRestrictedUser(app);
 
-
 const customUser = new CustomUser(app);
 app.controller.use(customUser);
 
-
-app.config.content.plugins['passport-oauth'] = {
+app.config.content.plugins["passport-oauth"] = {
   // List of the providers you want to use with passport
-  "strategies": {
-    "google": {
+  strategies: {
+    google: {
       // Strategy name for passport (eg. google-oauth20 while the name of the provider is google)
-      "passportStrategy": "google-oauth20",
-      // Credentials provided by the provider  
-      "credentials": {
-        "clientID": "248704848225-abvib4t5sh7jpolqurk39vcioklfdgo2.apps.googleusercontent.com",
-        "clientSecret": key_secret.clientsecret,
-      //  "callbackURL": "http://149.50.128.59:7512/_login/google",
-        "callbackURL": "http://localhost:1593/_login/app",
-        "profileFields": ["id", "name", "picture", "email", "gender"]
+      passportStrategy: "google-oauth20",
+      // Credentials provided by the provider
+      credentials: {
+        clientID:
+          "248704848225-abvib4t5sh7jpolqurk39vcioklfdgo2.apps.googleusercontent.com",
+        clientSecret: env.clientsecret,
+        //  "callbackURL": "http://149.50.128.59:7512/_login/google",
+        callbackURL: "http://localhost:1593/_login/app",
+        profileFields: ["id", "name", "picture", "email", "gender"],
       },
       // Attributes you want to persist in the user credentials object if the user doesn't exist
-      "persist": [
-        "last_name",
-        "first_name",
-        "email"
-      ],
+      persist: ["last_name", "first_name", "email"],
       // List of fields in the OAUTH 2.0 scope of access
-      "scope": [
-        "email",
-      ],
+      scope: ["email"],
       //Mapping of attributes to persist in the user persisted in Kuzzle
-      "kuzzleAttributesMapping": {
+      kuzzleAttributesMapping: {
         // will store the attribute "email" from oauth provider as "userEmail" into the user credentials object
-        "userMail": "email" 
+        userMail: "email",
       },
       // Attribute from the profile of the provider to use as unique identifier if you want to persist the user in Kuzzle
-      "identifierAttribute": "email"
-    }
+      identifierAttribute: "email",
+    },
   },
   // Profiles of the new persisted user
-  "defaultProfiles": [
-    "default"
-  ]
-}
+  defaultProfiles: ["default"],
+};
 
-app.config.content.security.restrictedProfileIds = ["default"]
-
-
-app.start();
+app.config.content.security.restrictedProfileIds = ["default"];
+app.start().then(() => {
+  app.configureSmtp(env.smtpConfig);
+});
--- lib/MyApplication.ts
+++ lib/MyApplication.ts
@@ -1,6 +1,7 @@
 import { Backend } from "kuzzle";
 import { PrometheusPlugin } from "kuzzle-plugin-prometheus";
 import PluginPassportOAuth from "kuzzle-plugin-auth-passport-oauth";
+import { HermesMessengerPlugin } from "kuzzle-plugin-hermes-messenger";
 
 export type MyApplicationConfig = {
   someValue: string;
@@ -14,7 +15,8 @@ export class MyApplication extends Backend {
   public configuration: MyApplicationConfig;
   private prometheusPlugin = new PrometheusPlugin();
   private passportOauthPlugin = new PluginPassportOAuth();
-  
+  private hermesMessengerPlugin = new HermesMessengerPlugin();
+
   get appConfig() {
     return this.config.content.application as MyApplicationConfig;
   }
@@ -31,12 +33,27 @@ export class MyApplication extends Backend {
 
     this.plugin.use(this.prometheusPlugin);
     this.plugin.use(this.passportOauthPlugin);
-
+    this.plugin.use(this.hermesMessengerPlugin);
   }
 
   async start() {
     await super.start();
 
     this.log.info("Application started");
+  }
+
+  async configureSmtp(config) {
+    await this.sdk.query({
+      account: config.account,
+      action: "addAccount",
+      body: {
+        defaultSender: config.defaultSender,
+        host: config.host,
+        pass: config.pass,
+        port: 465,
+        user: config.user,
+      },
+      controller: "hermes/smtp",
+    });
   }
 }
