4bfdd6e4b610046ffbd47e10ced1047189809c57
Martín Taritolay <martin.taritolay.nib@gmail.com>
2024-05-04 13:37:58 -0300

    Allows a user to validate their profile with the right token and userid

--- lib/local-users-handling/validate-users.ts
+++ lib/local-users-handling/validate-users.ts
@@ -25,12 +25,27 @@ export class CustomUser extends Controller {
     try {
         const {username, token} = request.input.args
         if(!(username && token)){
-          throw new BadRequestError("Invalid request");
+          throw new BadRequestError("Invalid request.");
+        }
+        // Get user data from user_kuid
+        const user_data = await this.app.sdk.security.getUser(username)
+        if (user_data._source.ValidationToken === token){
+          // If token is correct and the user has not yet been validated, then update profileIds of the corresponding user
+          if(user_data._source.profileIds.includes("profile-non-validated-users")){
+            console.log("The request is okay")
+            let body = {
+              profileIds: ['profile-validated-users']
+            }
+            this.app.sdk.security.updateUser(username, body);
+          } else{
+            throw new BadRequestError("Error in user validation. Maybe the user is already validated?")
+          }
+        } else{
+          throw new BadRequestError("Invalid token")
         }
     } catch (e) {
       console.error(e);
       throw new BadRequestError("Invalid request")
-      return
     }
     return
   }
